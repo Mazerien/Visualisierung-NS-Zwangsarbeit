ec3889685a6b08e4602ffc74c93fb0c555e5ce3d
SootDan <124447854+SootDan@users.noreply.github.com>
2026-01-05 16:55:40 +0100

    feat: colors in europe; wip: deal with micronations

--- backend/osm.py
+++ backend/osm.py
@@ -11,25 +11,34 @@ import random
 WORLD_1938: str = "https://raw.githubusercontent.com/aourednik/historical-basemaps/refs/heads/master/geojson/world_1938.geojson"
 WORLD_2026: str = "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/world_countries.json"
 
+# TODO: Make this code less bad.
+COUNTRY_COLORS = {
+}
+COLOR_ONE = ["Germany", "USSR", "Spain", "United Kingdom", "Turkey", "Hungary"]
+COLOR_TWO = ["Italy", "Finland", "Yugoslavia", "Netherlands",
+             "Czechoslovakia", "Portugal", "Bulgaria", "Libya"]
+COLOR_THREE = ["Poland", "Romania", "Greece", "France", "Norway",
+               "Estonia", "Iran", "Estonia", "Syria (France)", "Tunisia"]
+COLOR_FOUR = ["Sweden", "Switzerland", "Belgium",
+              "Ireland", "Lithuania", "Algeria (France)"]
+COLOR_FIVE = ["Denmark", "Latvia", "Iraq", "Luxembourg",
+              "Armenia", "Albania", "Morocco (France)", "Mesopotamia"]
+for s in COLOR_ONE:
+    COUNTRY_COLORS[s] = "#fffdc1"
+for s in COLOR_TWO:
+    COUNTRY_COLORS[s] = "#ffcdcd"
+for s in COLOR_THREE:
+    COUNTRY_COLORS[s] = "#e9f2ae"
+for s in COLOR_FOUR:
+    COUNTRY_COLORS[s] = "#ffdfaa"
+for s in COLOR_FIVE:
+    COUNTRY_COLORS[s] = "#eccff2"
+
 
 class ZoomLevel(Enum):
     MINIMUM = 0
     MEDIUM = 1
     MAXIMUM = 2
-
-# TODO: Figure out how to do read the GeoJSON to make a Chloropeth map out of it
-# country_borders = pd.read_json(
-#     requests.get(
-#         "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/world_countries.json").text
-# )
-# print(country_borders)
-
-
-# def style_function(feature):
-#     country_name = feature["properties"]["NAME"]
-#     return {
-#         "fillColor": country_borders.get(country_name, "gray")
-#     }
 
 
 class OSM:
@@ -93,17 +102,60 @@ class OSM:
 
         # TODO: Make this more modular
         # TODO: Add given colours for the countries; not randomized.
-        def style_function(_):
+        def style_function(feature):
+            country = feature["properties"]["NAME"]
+            fill_color = f"#5BCEFA"
+            color = "black"
+            weight = 1
+            fill_opacity = 0.75
+
+            if country in COUNTRY_COLORS:
+                return {
+                    "fillColor": COUNTRY_COLORS[country],
+                    "color": color,
+                    "weight": weight,
+                    "fillOpacity": fill_opacity,
+                }
+            #if country is not None:
+            #    print(country)
             return {
                 "fillColor": f'#{random.randint(0, 0xFFFFFF):06x}',
-                "color": "black",
-                "weight": 1,
-                "fillOpacity": 0.25,
+                "color": color,
+                "weight": weight,
+                "fillOpacity": fill_opacity,
             }
 
-        folium.GeoJson(self.geo_json, name="1938", style_function=style_function).add_to(m)
+        # tooltip = folium.GeoJsonTooltip(
+        #     fields=["NAME"],
+        #     aliases=["Country:"],
+        #     localize=True,
+        #     sticky=True,
+        #     labels=True,
+        #     style="""
+        #     background-color: #F0EFEF;
+        #     border: 2px solid black;
+        #     border-radius: 3px;
+        #     box-shadow: 3px;
+        #     """,
+        #     max_width=800,
+        # )
+
+        # TODO: Properly add country names to the map. Right now the title is weirdly placed.
+        # for feature in self.geo_json["features"]:
+        #     name = feature["properties"]["NAME"]
+        #     if not name == "None" or name is not None:
+        #         coords = feature["geometry"]["coordinates"][0][0][0]
+        #         folium.Marker(
+        #             location=[coords[1], coords[0]],
+        #             icon=folium.features.DivIcon(
+        #                 # TODO: Make this more modular
+        #                 html=f'<div style="font-size: 12pt; color: black; text-shadow: white 0.1em 0.1em 0.1em;">{name}</div>'
+        #             )
+        #         ).add_to(m)
+
+        # folium.GeoJson(self.geo_json, name="1938", style_function=style_function, tooltip=tooltip).add_to(m)
+        folium.GeoJson(self.geo_json, name="1938",
+                       style_function=style_function).add_to(m)
         folium.LayerControl().add_to(m)
 
-        #self.geo_json = "https://raw.githubusercontent.com/aourednik/historical-basemaps/refs/heads/master/geojson/world_1938.geojson"
-        #folium.GeoJson(self.geo_json, name="2025").add_to(m)
         return m.get_root()._repr_html_()
