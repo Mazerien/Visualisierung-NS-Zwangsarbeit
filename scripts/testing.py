dcd26924e423bcdf5f055673bfc824b43acb4ebb
SootDan <124447854+SootDan@users.noreply.github.com>
2026-05-04 22:19:54 +0200

    fix: changed test url

--- scripts/testing.py
+++ scripts/testing.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 
-URL = "http://localhost:5000"
+URL = "https://flask.p-qsvcne.project.space"
 
 def test_connection():
     res = requests.get(f"{URL}/api/hello")
@@ -16,5 +16,4 @@ def test_osm_no_params():
 def test_osm_params():
     res = requests.get(f"{URL}/api/osm?zoom_level=2")
     assert res.status_code == 200
-
 
