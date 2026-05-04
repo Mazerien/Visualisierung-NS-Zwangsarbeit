ba3fc6b13cd51e00b515b5303f14dcb0578ec676
SootDan <124447854+SootDan@users.noreply.github.com>
2026-05-04 22:12:09 +0200

    test: unit test workflow

--- scripts/testing.py
+++ scripts/testing.py
@@ -11,3 +11,10 @@ def test_connection():
 def test_osm_no_params():
     res = requests.get(f"{URL}/api/osm")
     assert res.status_code == 200
+
+
+def test_osm_params():
+    res = requests.get(f"{URL}/api/osm?zoom_level=2")
+    assert res.status_code == 200
+
+
