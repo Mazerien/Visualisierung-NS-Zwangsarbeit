83a3d7e6043513120cf99d442b5a7bb78a067cfd
SootDan <124447854+SootDan@users.noreply.github.com>
2026-05-04 22:42:23 +0200

    test: unit test

--- scripts/testing.py
+++ scripts/testing.py
@@ -1,7 +1,8 @@
+from backend.app import app
 import pytest
 import requests
 
-URL = "https://flask.p-qsvcne.project.space"
+URL = "http://localhost:5000"
 
 def test_connection():
     res = requests.get(f"{URL}/api/hello")
@@ -16,4 +17,14 @@ def test_osm_no_params():
 def test_osm_params():
     res = requests.get(f"{URL}/api/osm?zoom_level=2")
     assert res.status_code == 200
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
 
